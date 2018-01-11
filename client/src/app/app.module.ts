a60c064ffe2659d2f6a96bf32aa5d592590c3527
Goyal, Ayush1 [ICG-IT] <ag65856@imcnam.ssmb.com>
2018-01-11 18:50:30 +0530

    added methods in chat compo and exported materail modules

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { ChatModule } from './chat/chat.module';
 
 
 @NgModule({
@@ -14,7 +16,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    ChatModule
   ],
   providers: [],
   bootstrap: [AppComponent]
