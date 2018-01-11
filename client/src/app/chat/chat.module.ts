72b7e82994403d62edfa3212ea8b27386efd4fd5
Goyal, Ayush1 [ICG-IT] <ag65856@imcnam.ssmb.com>
2018-01-11 19:12:09 +0530

    added template for chat comp

--- client/src/app/chat/chat.module.ts
+++ client/src/app/chat/chat.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChatComponent } from './chat.component';
 import { SocketService } from './shared/services/socket.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../shared/material/material.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule
   ],
   declarations: [ChatComponent],
   providers: [SocketService]
